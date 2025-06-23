b939528ac56fad09f7b7ce893b33110e867ba0e4
„Enes <enes.gnay@yahoo.de>
2025-06-23 17:12:21 +0200

    First Day done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
 
-    // 4. shuffleQuestions()
 
-    // 5. checkAnswer(answer)
+    moveToNextQuestion() {
+        ++this.currentQuestionIndex
+    }
 
-    // 6. hasEnded()
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
+
+    checkAnswer(answer) {
+    if(answer===this.questions[this.currentQuestionIndex].answer){
+        ++this.correctAnswers
+    }
+    }
+
+     hasEnded(){
+        if(this.currentQuestionIndex<this.questions.length)
+            return false
+        else(this.currentQuestionIndex=this.questions.length)
+        return true
+     }
 }
