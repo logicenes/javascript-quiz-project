547fd3510ec4991fcfba8b243acb6fcaca1be132
„Enes <enes.gnay@yahoo.de>
2025-06-24 15:19:11 +0200

    Implemented averageDifficulty Method

--- src/quiz.js
+++ src/quiz.js
@@ -24,15 +24,33 @@ class Quiz {
     }
 
     checkAnswer(answer) {
-    if(answer===this.questions[this.currentQuestionIndex].answer){
-        ++this.correctAnswers
-    }
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
+            ++this.correctAnswers
+        }
     }
 
-     hasEnded(){
-        if(this.currentQuestionIndex<this.questions.length)
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length)
             return false
-        else(this.currentQuestionIndex=this.questions.length)
+        else (this.currentQuestionIndex = this.questions.length)
         return true
-     }
+    }
+
+    filterQuestionsByDifficulty(difficulty) {
+        this.difficulty = difficulty;
+        if (difficulty >= 1 && difficulty <= 3) {
+            this.questions = this.questions.filter(question => question.difficulty === difficulty)
+        }
+    }
+    averageDifficulty() {
+        const result = this.questions.reduce((acc, question) => {
+            if (question.difficulty !== undefined) {
+                acc.sum += question.difficulty;
+                acc.count += 1;
+            }
+            return acc;
+        }, { sum: 0, count: 0 });
+        const avg = result.count > 0 ? result.sum / result.count : 0;
+        return avg;
+    }
 }
